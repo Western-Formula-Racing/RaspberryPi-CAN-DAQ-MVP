6e6448fb4e4339b4cd3d45cfadf28f6cbb214248
Brody Smith <bsmit272@uwo.ca>
2023-04-28 12:35:43 -0400

    Add MQTT functionality (InfluxDB not autoupdating on Grafana)

--- pythonAPI/canInterface.py
+++ pythonAPI/canInterface.py
@@ -33,12 +33,13 @@ mqttClient = mqtt.Client(clientName)
 mqttClient.on_connect = on_connect
 mqttClient.on_publish = on_publish
 
-mqttClient.connect("localhost")
+mqttClient.connect("localhost", 1883)
 
 # CAN Bus stuff
 filters = [
     # the mask is applied to the filter to determine which bits in the ID to check (https://forum.arduino.cc/t/filtering-and-masking-in-can-bus/586068/3)
-    {"can_id": 0x036, "can_mask": 0xFFF, "extended": False}
+    {"can_id": 0x036, "can_mask": 0xFFF, "extended": False},
+    {"can_id": 0x023, "can_mask": 0xFFF, "extended": False}
 ]
 # start an interface using the socketcan interface, using the can0 physical device at a 500KHz frequency with the above filters
 #bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000, can_filters=filters)
@@ -50,38 +51,38 @@ print("reading Can Bus:")
 for msg in bus:
     #os.system('clear')
 
-    hub1 = [0 for x in range(8)]
+    hub_data = [0 for x in range(8)]
+    hub_no = 0
 
-    if msg.arbitration_id == 0x36: 
+    if msg.arbitration_id == 0x36 or msg.arbitration_id == 0x23: 
         if len(msg.data) > 8 or len(msg.data) < 0: 
             # not really possible if the MTU of the CAN interface is less than or equal to 16
             raise ValueError("Invalid message size")
+        if msg.arbitration_id == 0x36:
+            hub_no = 2
+        else: 
+            hub_no = 1
         for i, byte in enumerate(msg.data):
-            hub1[i] = byte
+            hub_data[i] = byte
 
     time = datetime.datetime.utcnow()
-
+    this_board_name = CAN_ID_TO_SENSOR_BOARD_LUT[msg.arbitration_id]["board_name"]
     body = [{ # I don't know why this object is an array https://influxdb-python.readthedocs.io/en/latest/examples.html
-        "measurement": CAN_ID_TO_SENSOR_BOARD_LUT[msg.arbitration_id]["board_name"],
+        "measurement": this_board_name,
         "time": time,
         "fields": {}
     }]
 
     for i, sensors in enumerate(CAN_ID_TO_SENSOR_BOARD_LUT[msg.arbitration_id]["sensors"]):
-        print("this is sensors:")
-        body[0]["fields"][sensors["sensor_name"]] = hub1[i]
-
+        this_sensor_name = sensors["sensor_name"] # constant lookup bc hashmap i think so doesnt matter but more readable  
+        body[0]["fields"][this_sensor_name] = hub_data[i]
+        # "SensorBoardN/sensorM"
+        mqtt_str = f"{this_board_name}/{this_sensor_name}"
+        mqttClient.publish(mqtt_str, hub_data[i])
+        
     ifclient.write_points(body)
-    mqttClient.publish("SensorBoard1/sensor1", hub1[0])
-    mqttClient.publish("SensorBoard1/sensor2", hub1[1])
-    mqttClient.publish("SensorBoard1/sensor3", hub1[2])
-    mqttClient.publish("SensorBoard1/sensor4", hub1[3])
-    mqttClient.publish("SensorBoard1/sensor5", hub1[4])
-    mqttClient.publish("SensorBoard1/sensor6", hub1[5])
-    mqttClient.publish("SensorBoard1/sensor7", hub1[6])
-    mqttClient.publish("SensorBoard1/sensor8", hub1[7])
 
-    print("Sensor Hub 1:")
+    print(f"Sensor Hub {hub_no}:")
     for sensors in body[0]["fields"]:
         print(f"{sensors}: {body[0]['fields'][sensors]}")
     
