2cea4c4082b10f4d62666b8cf94eae2e308b2a47
Brody Smith <bsmit272@uwo.ca>
2023-06-01 10:36:46 -0400

    Asynchronously read two CAN buses

--- pythonAPI/canInterface.py
+++ pythonAPI/canInterface.py
@@ -16,10 +16,11 @@ ifdb = "home"
 ifhost = "127.0.0.1"
 ifport = 8086
 graphName = "SensorBoard1"
-ifclient = InfluxDBClient(host='127.0.0.1', port=8086,
-                          username='grafana', password='admin', database='home')
+ifclient = InfluxDBClient(
+    host="127.0.0.1", port=8086, username="grafana", password="admin", database="home"
+)
 
-db = cantools.database.load_file('dbc/demo_sensorboard.dbc')
+db = cantools.database.load_file("dbc/demo_sensorboard.dbc")
 
 print("db messages:")
 print(db.messages)
@@ -27,8 +28,9 @@ print(db.messages)
 # MQTT publisher setup
 clientName = "daq"
 
+
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
 
 
 def on_publish(client, userdata, result):
@@ -46,56 +48,64 @@ mqttClient.connect("localhost", 1883)
 # CAN Bus stuff
 def decode_and_broadcast(msg: can.Message) -> None:
     print(msg)
-    print("Just printed\n")
-        # decoded = self.messageFormat.message_decode(msg.arbitration_id, msg.data)
-        # board_name = self.messageFormat.get_message_by_frame_id(msg.arbitration_id).name
-        # body = [{ # I don't know why this object is an array https://influxdb-python.readthedocs.io/en/latest/examples.html
-        #     "measurement": board_name, 
-        #     "time": datetime.datetime.utcnow(),
-        #     "fields": decoded
-        # }]
-        # self.ifClient.write_points(body)
-        # for reading in decoded:
-        #     mqttStr = f"{board_name}/{reading}"
-        #     self.mqttClient.publish(mqttStr, decoded[reading])
+    # decoded = db.decode_message(msg.arbitration_id, msg.data)
+    # board_name = db.get_message_by_frame_id(msg.arbitration_id).name
+    # body = [{ # I don't know why this object is an array https://influxdb-python.readthedocs.io/en/latest/examples.html
+    #     "measurement": board_name,
+    #     "time": datetime.datetime.utcnow(),
+    #     "fields": decoded
+    # }]
+    # ifclient.write_points(body)
+    # for reading in decoded:
+    #     mqttStr = f"{board_name}/{reading}"
+    #     self.mqttClient.publish(mqttStr, decoded[reading])
+
+
+async def blocking_reader(reader: can.AsyncBufferedReader) -> None:
+    while True:
+        await reader.get_message()
 
 
 filters = [
     # the mask is applied to the filter to determine which bits in the ID to check (https://forum.arduino.cc/t/filtering-and-masking-in-can-bus/586068/3)
-    {"can_id": 0x036, "can_mask": 0xFFF, "extended": False},
-    {"can_id": 0x023, "can_mask": 0xFFF, "extended": False}
+    {"can_id": 0x103, "can_mask": 0xFFF, "extended": False},  # sensor board 1
+    {"can_id": 0x104, "can_mask": 0xFFF, "extended": False},  # sensor board 2
 ]
 # start an interface using the socketcan interface, using the can0 physical device at a 500KHz frequency with the above filters
 
-# Use the virtual CAN interface in lieu of a physical connection 
-bus_one = can.interface.Bus(bustype='socketcan', channel='vcan0')
-bus_two = can.interface.Bus(bustype='socketcan', channel='vcan1')
+# Use the virtual CAN interface in lieu of a physical connection
+bus_one = can.interface.Bus(bustype="socketcan", channel="vcan0", filter=filters[0])
+bus_two = can.interface.Bus(bustype="socketcan", channel="vcan1", filter=filters[1])
 
 async def main() -> None:
-    bus_one_reader = can.AsyncBufferedReader()
-    bus_two_reader = can.AsyncBufferedReader()
+    reader_bus_one = can.AsyncBufferedReader()
+    reader_bus_two = can.AsyncBufferedReader()
+
+    # Logger can be used to log to Influx, it just has to be made (see logic in listeners.py)
     logger = can.Logger("logfile.asc")
 
-    bus_one_listeners: List[MessageRecipient] = [
+    listeners_bus_one: List[MessageRecipient] = [
         decode_and_broadcast,  # Callback function
-        bus_one_reader,  # AsyncBufferedReader() listener
+        reader_bus_one,  # AsyncBufferedReader() listener
         logger,  # Regular Listener object
     ]
 
-    bus_two_listeners: List[MessageRecipient] = [
-        decode_and_broadcast,  # Callback function
-        bus_two_reader,  # AsyncBufferedReader() listener
-        logger,  # Regular Listener object
+    listeners_bus_two: List[MessageRecipient] = [
+        decode_and_broadcast, 
+        reader_bus_two,  
+        logger, 
     ]
 
     # Create Notifier with an explicit loop to use for scheduling of callbacks
     loop = asyncio.get_running_loop()
-    notifier_bus_one = can.Notifier(bus_one, bus_one_listeners, loop=loop)
-    notifier_bus_two = can.Notifier(bus_two, bus_two_listeners, loop=loop)
+    notifier_bus_one = can.Notifier(bus_one, listeners_bus_one, loop=loop)
+    notifier_bus_two = can.Notifier(bus_two, listeners_bus_two, loop=loop)
 
-    while True:
-        msg = await bus_one_reader.get_message()
-        msg = await bus_two_reader.get_message()
+    # Right now, this will be running until the car is turned off, so no end 
+    await asyncio.gather(
+        blocking_reader(reader_bus_one),
+        blocking_reader(reader_bus_two),
+    )
 
 
 if __name__ == "__main__":
